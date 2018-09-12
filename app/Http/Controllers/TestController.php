dbdf0cf5ec173157424559b9deb6dabbc6cc9917
limq <limq@cgnfs.com.cn>
2018-09-12 16:44:28 +0800

    composer require barryvdh/laravel-debugbar --dev 安装 debugbar //可以在 dont-discover 配置项中通过通配符 * 禁止所有扩展包的自动发现功能 Laravel 5.5+: If you don't use auto-discovery, add the ServiceProvider to the providers array in config/app.php
    Barryvdh\Debugbar\ServiceProvider::class,
    If you want to use the facade to log messages, add this to your facades in app.php:
    
    'Debugbar' => Barryvdh\Debugbar\Facade::class,

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use BarryDebugbar;
 
 class TestController extends Controller
 {
-    //
+
+
+    public function test(Request $request)
+    {
+        //return $request->input('name','jack');
+        $name = $request->input('name','jack');
+
+        BarryDebugbar::info($name);
+        return view('welcome',compact('name'));
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    {{ ucfirst($name) }}'s Laravel
                 </div>
 
                 <div class="links">
