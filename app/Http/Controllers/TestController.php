2745a0762867e7154ae03597bfd28c2029da56ad
limq <limq@cgnfs.com.cn>
2018-09-13 10:12:39 +0800

    包自动发现 ideHelper

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -4,17 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Debugbar;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\View;
 
 class TestController extends Controller
 {
 
 
+    /**
+     * 包自动发现Test
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function test(Request $request)
     {
-        //return $request->input('name','jack');
-        $name = $request->input('name', 'jack');
+        //$res = DB::
+        //$res = DB::where('name','=','jack')->()
+        ////return $request->input('name','jack');
+        //$name = $request->input('name', 'jack');
+        //
+        //Debugbar::info($name . date('Y-m-d H:i:s') . '访问了页面！');
+        //return view('welcome', compact('name'));
 
-        Debugbar::info($name . date('Y-m-d H:i:s') . '访问了页面！');
-        return view('welcome', compact('name'));
     }
 }
